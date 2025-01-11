8b1459051d5a1e21146619be6e8c16623bd84fe5
S33B007 <108036615+S33B007@users.noreply.github.com>
2025-01-11 09:44:48 -0500

    Create range_test.py

---
+++ Week 4 - Functions/Chapter Exercises/range_test.py
@@ -0,0 +1,20 @@
+# Write a function named range_test that takes an integer 
+# as an argument and returns True if the number is within 
+# the range 1 to 555 (not inclusive, i.e. neither 1 nor 555 are in range). 
+# Otherwise return False
+
+def range_test(num):
+    count = 0
+    for i in range(2,555,1):
+        while count < i:
+            if i == num:
+                return True
+            else:
+                count += 1
+
+
+num = int(input("Enter a number: \n"))
+if range_test(num):
+    print( "{:d} is in range.".format(num))
+else:
+    print("The number you entered is outside the range!")
